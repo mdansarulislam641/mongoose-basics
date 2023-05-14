62ad0128291a80093fc6c8d97e89302f9216e568
mdansarulislam641 <mdansarulislam641.com>
2023-05-14 07:43:33 +0600

    data saved on mogdodb using mongoose and typescript

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 
-import cors from 'cors';
 import express ,{ Application , Request , Response } from 'express';
+import { Schema, model } from 'mongoose';
+import cors from 'cors';
 
 const app : Application = express()
 // using cors policy 
@@ -24,13 +25,85 @@ app.get('/', (req : Request, res : Response) => {
         role : 'student';
         name : {
             firstName : string ;
-            middleName : string ;
-            lastName : string 
+            middleName? : string ;
+            lastName? : string 
         };
         gender : 'male' | 'female';
         email?:string ;
         birthDate?: string ;
-    }
+        presentAddress : string ;
+        permanentAddress : string ;
+        emergencyContact: number
+    };
+
+    // creating Schema 
+    const userSchema = new Schema<IUser>({
+        id : {
+            type: Number ,
+            required: true ,
+            unique : true
+        } ,
+        role: {
+            type: String ,
+            required: true
+        },
+        name : {
+            firstName: {
+                type : String ,
+                required: true
+            } ,
+            lastName: {
+                type: String
+            } ,
+            middleName: {
+                type : String
+             }
+        },
+        birthDate: {
+            type: String 
+        } ,
+        gender: {
+            type : String,
+            enum: ['male', 'female m']
+        },
+        email:{
+            type: String
+        }, 
+        presentAddress:{
+            type: String ,
+            required: true
+        },
+        permanentAddress:{
+            type : String 
+        },
+        emergencyContact: {
+            type : Number
+        }
+    });
+
+    // creating model 
+    const User = model<IUser>('usersMongoose', userSchema);
+
+    // creating user interface 
+   const createUserToDb = async() =>{
+    const user = new User ({
+        id: 3 ,
+        role: 'student',
+        name :{
+            firstName:'Md Ansarul',
+            lastName: "Islam"
+        },
+        birthDate: '20-12-2023',
+        email: 'abc@gmail.com',
+        presentAddress :"Thakurgaon",
+        permanentAddress : "Thangagonj",
+        gender: 'male',
+        
+    });
+    await user.save()
+   };
+
+   createUserToDb();
 });
 
 export default app ;
