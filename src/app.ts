a33ab4d52f5cf462f119b3c80001a835f7dde186
mdansarulislam641 <mdansarulislam641.com>
2023-05-14 06:43:58 +0600

    mongoose interface make done

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,27 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req : Request, res : Response) => {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    // data store into mongodb 
+    /* 
+    step -1 : Interface 
+    step -2 : Schema
+    step -3 : Model 
+    step -4 : Database Query
+    */
+//    creating Interface of user 
+    interface IUser {
+        id : number ;
+        role : 'student';
+        name : {
+            firstName : string ;
+            middleName : string ;
+            lastName : string 
+        };
+        gender : 'male' | 'female';
+        email?:string ;
+        birthDate?: string ;
+    }
 });
 
 export default app ;
